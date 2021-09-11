a849addf5e9aa7b6f3cc03973206f753171c4f6c
zinaelnahel <zinamahmoud1989@gmail.com>
2021-09-11 18:37:59 +0200

    add profile

---
+++ src/Nav.js
@@ -0,0 +1,78 @@
+import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import "./App.css";
+import Projects from "./Projects";
+import Skills from "./Skills";
+
+import Contact from "./Contact";
+export default function Nav() {
+  const [value, setValue] = React.useState();
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box p={0}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+  };
+
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  }
+
+  return (
+    <div>
+      <AppBar>
+        <Tabs
+          indicatorColor="white"
+          value={value}
+          onChange={handleChange}
+          aria-label="simple tabs example"
+        >
+          <Tab label="Skills" {...a11yProps(0)} />
+          <Tab label="Projects" {...a11yProps(1)} />
+          <Tab label="Contact Me" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <Skills />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <Projects />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <Contact />
+      </TabPanel>
+    </div>
+  );
+}
---
+++ src/Skills.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import "./App.css";
+import Paper from "@material-ui/core/Paper";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(2),
+      width: theme.spacing(40),
+      height: theme.spacing(20),
+    },
+  },
+}));
+function Skills() {
+  const classes = useStyles();
+  return (
+    <header className="App-header">
+      <h6 style={{ marginTop: "0" }}>My skills set includes : </h6>
+      <div className={classes.root}>
+        <Paper style={{ backgroundColor: "#95DAC1" }}>
+          <Typography
+            variant="h6"
+            style={{ color: "#6F69AC", textAlign: "center" }}
+          >
+            👩🏻‍💻 Front-End
+            <ul
+              style={{
+                listStyleType: "none",
+                textAlign: "left",
+                fontSize: "15px",
+              }}
+            >
+              <li>HTML / CSS / Git</li>
+              <li>JavaScript / React </li>
+              <li>Bootstrap / Material UI </li>
+              <li>UX / UI / Scrum </li>
+            </ul>
+          </Typography>
+        </Paper>
+        <Paper style={{ backgroundColor: "#95DAC1" }}>
+          {" "}
+          <Typography
+            variant="h6"
+            style={{ color: "#6F69AC", textAlign: "center" }}
+          >
+            💻 Back-End
+            <ul
+              style={{
+                listStyleType: "none",
+                textAlign: "left",
+                fontSize: "15px",
+              }}
+            >
+              <li>Node.js / Express.js</li>
+              <li>MongoDB / Mongoose</li>
+              <li>SQL / PostgreSQL</li>
+              <li>API</li>
+            </ul>
+          </Typography>
+        </Paper>
+      </div>
+    </header>
+  );
+}
+
+export default Skills;
